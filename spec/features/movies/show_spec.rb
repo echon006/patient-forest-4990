87e2a43710b6ca13acd2a2b9819600b09b6ac057
Eric Chon <echon92@gmail.com>
2021-12-13 14:52:39 -0700

    added to test to show actors name, pending to make youngest to oldest

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -9,7 +9,18 @@ RSpec.describe 'Movies show page' do
     @movie_2 = @studio_1.movies.create(title: "Movie 2", creation_year: 1991, genre: "Comedy")
     @movie_3 = @studio_2.movies.create(title: "Movie 3", creation_year: 1992, genre: "Horror")
     @movie_4 = @studio_2.movies.create(title: "Movie 4", creation_year: 1993, genre: "Sci-Fi")
+
+    @actor1 = @movie_1.actors.create(name: "actor 1", age: 20)
+    @actor2 = @movie_1.actors.create(name: "actor 2", age: 20)
+    @actor3 = @movie_1.actors.create(name: "actor 3", age: 20)
+    @actor4 = @movie_1.actors.create(name: "actor 4", age: 20)
+
+    @movie_actor1 = MovieActor.create!(actor: @actor1, movie: @movie_1)
+    @movie_actor2 = MovieActor.create!(actor: @actor2, movie: @movie_1)
+
   end
+
+
 
   it "shows all the movies and their information" do
     visit "/movies/#{@movie_1.id}"
@@ -17,5 +28,7 @@ RSpec.describe 'Movies show page' do
     expect(page).to have_content(@movie_1.title)
     expect(page).to have_content(@movie_1.creation_year)
     expect(page).to have_content(@movie_1.genre)
+    expect(page).to have_content(@actor1.name)
+
   end
 end
